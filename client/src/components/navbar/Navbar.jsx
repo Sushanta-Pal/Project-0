267858ddf2d6d39c1294b8da785eff037ff838c1
Sushanta-Pal <sp0071331@gmail.com>
2024-07-29 14:41:03 +0530

    new change

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { authActions } from '../../store';
-import './navBar.css';
+ import './navBar.css';
 import { SiMastodon } from "react-icons/si";
 
 const Navbar = () => {
